b81f3a4f76b3009a4f45364f3ec551effd54fb3e
ersincebi <ersincebi.1995@gmail.com>
2019-10-29 17:21:08 +0300

    template integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './layouts/blanklayout/blanklayout.component.html',
+	styleUrls: [ ]
 })
-export class AppComponent {
-  title = 'whentheringbells';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { TopbarComponent } from './layouts/topbar/topbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { BlanklayoutComponent } from './layouts/blanklayout/blanklayout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		SigninComponent,
+		TopbarComponent,
+		SidebarComponent,
+		BlanklayoutComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
