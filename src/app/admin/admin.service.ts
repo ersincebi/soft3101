c85fa2496bb5066dd87ea2a7763d39b7edcd8d11
Çağatay Demircan <cagataydmr1@gmail.com>
2019-12-22 14:10:53 +0300

    AdminChangesv2

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { map } from 'rxjs/operators';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-
+    
+    
+    
   constructor(private db : AngularFireDatabase) { }
-
+   
   getRequests(user: firebase.User) {
-    return  this.db.list('/Requests/').snapshotChanges();
+      
+    return  this.db.list('/Requests/1').snapshotChanges();
+    
+    
 }
+
+   update(){
+     
+   }
+
+   updateFirst(key){
+       this.db.object('Requests/'+key+'/').set('false');
+   }
+
+   
 }
